713147a092e21e98d3081fd8f5856e09a900362d
Giovanni Rivera <grivera64@toromail.csudh.edu>
2021-07-17 13:12:13 -0700

    :sparkles: feat: add moneyball_equation.py

---
+++ project3/moneyball_equation.py
@@ -0,0 +1,87 @@
+import openpyxl as excel
+
+def main():
+
+    # Welcome message:
+
+    print("Welcome to the Moneyball Equation application!")
+
+    while True:
+
+        file_path = input("Please enter the absolute/relative path to your file: ")
+
+        if (file_path.lower() == "q"):
+
+            print("Thank you for using Moneyball Equation application!\n\nQuitting...")
+            exit(1)
+
+        # Open excel sheet
+        print("Openning file...")
+        
+        try:
+
+            wb = excel.load_workbook(file_path)
+            print("Opened file successfully!\n")
+            break
+
+        except FileNotFoundError:
+
+            print("File not found! Please try again...\n(Note: Press \'q\' to quit)\n")
+        
+        except excel.utils.exceptions.InvalidFileException:
+
+            print("File format not supported! Please try again...\n(Note: Press \'q\' to quit)\n")
+
+    # TODO Read Cell Data from excel sheet ws['RC'] where R == row and C == column
+
+    # Reference:
+    # Columns:
+    # A ==> Team
+    # B ==> Games Played
+    # C ==> Runs Scored
+    # D ==> Runs Allowed
+    # E ==> Wins
+    # F ==> Losses
+    # G ==> W-L% (to be edited)
+    # Rows:
+    # 1 - the titles
+    # 2-31 are valid teams
+
+    # Iterate through each worksheet
+    for name in wb.sheetnames:
+
+        print(f"Writing to {name}...")
+
+        # Enter the worksheet
+        ws = wb[name]
+        curr_percentage: float
+
+        # For each row in the sheet
+        for row_index in range(2, 32):
+
+            # Find the current team's values
+            wins = ws["E" + str(row_index)].value
+            games_played = ws["B" + str(row_index)].value
+
+            # Use the Pythagorean Expectation Formula
+            percentage = (float(wins) / float(games_played))
+            ws["G" + str(row_index)] = percentage
+            ws["G" + str(row_index)].number_format = "0.00%"
+
+    # Save to a new file called similarily but append '_edited'
+    # before the .xlsx extension
+    save_path: str = file_path[0:-5:1] + '_edited.xlsx'
+    print(f"\nDone!\nSaving to {save_path}...")
+    wb.save(save_path)
+
+    # Close excel sheet for security reasons
+    wb.close()
+
+    print("\nExitting...\n")
+
+    pass
+
+# Driver of the file
+if __name__ == "__main__":
+
+    main()
